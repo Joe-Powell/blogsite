d5751984bd6a51045ad67479c4f13de599e6920b
Joe Powell <joe_773@yahoo.com>
2020-10-07 10:42:32 -0500

    added changes

--- webDev.php
+++ webDev.php
@@ -26,8 +26,7 @@
     <h2></h2>
     <div class="overlay">
       <h4>
-      After learning React, I thought it was gonna be smooth sailing from here,
-      but Redux was a whole new technology to learn. In this blog I will 
+    In this blog I will 
       talk about how I learned Redux and how it works.
         <a href='./blogs/reactAndRedux.php' target="_blank"> Read more</a>
       </h4>
@@ -41,7 +40,7 @@
     <h2></h2>
     <div class="overlay">
       <h4> This is a CRUD app I made using Php.<span> You can register an account, log in, leave a post, upload photos, and much more.</span>
-        <a href='./blogs/crudBlog.php' target="_blank"> Read more</a>
+        <a href='#' target="_blank"> Read more</a>
       </h4>
 
     </div>
@@ -53,7 +52,7 @@
     <h2></h2>
     <div class="overlay">
       <h4> This is a CRUD app I made using Php.<span> You can register an account, log in, leave a post, upload photos, and much more.</span>
-        <a href='./blogs/crudBlog.php'> Read more</a>
+        <a href='#'> Read more</a>
       </h4>
 
     </div>
